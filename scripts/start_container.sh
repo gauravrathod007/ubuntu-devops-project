00ca8bbdaaef76b48a2432554d01e241e2edadfb
root <root@ip-172-31-83-190.ec2.internal>
2023-09-25 05:57:22 +0000

    add 2nd version of app

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
-
+docker pull gauravdkr7/simple-python-flask-app
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 gauravdkr7/simple-python-flask-app
+
