f3c271c4daea009901625a5e0042dd05fc2b4fa6
root <root@ip-172-31-83-190.ec2.internal>
2023-09-26 04:25:59 +0000

    deleted all files

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull gauravdkr7/simple-python-flask-app
-# Run the Docker image as a container
-docker run -d -p 5000:5000 gauravdkr7/simple-python-flask-app
-
