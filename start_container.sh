5d16042abd73b2f70151caef3b24fa4add97840b
root <root@ip-172-31-83-190.ec2.internal>
2023-09-27 05:56:17 +0000

    changes start_container.sh file

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull gauravdkr7/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 gauravdkr7/simple-python-flask-app
+
